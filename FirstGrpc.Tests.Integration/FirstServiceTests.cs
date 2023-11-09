b1eaf15cbde1916367157006e93b905f118361d1
John Cassidy <jpcassidy@outlook.com>
2023-11-09 08:10:49 -0500

    Create FirstServiceTests Integration Test

---
+++ FirstGrpc.Tests.Integration/FirstServiceTests.cs
@@ -0,0 +1,25 @@
+using Basics;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace FirstGrpc.Tests.Integration {
+    public class FirstServiceTests : IClassFixture<MyFactory<Program>> {
+        private readonly MyFactory<Program> factory;
+
+        public FirstServiceTests(MyFactory<Program> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public void GetUnaryMessage() {
+            // Arrange
+            var client = factory.CreateGrpcClient();
+            var expectedResponse = new Response { Message = "message from server" };
+            // Act
+            var actualResponse = client.Unary(new Request { Content = "message" });
+            // Assert
+            actualResponse.Should().BeEquivalentTo(expectedResponse);
+        }
+    }
+}
